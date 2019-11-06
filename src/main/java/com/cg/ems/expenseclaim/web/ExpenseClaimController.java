29845605b047cbd349cfee53300bcfeaf453271e
pritam <pritam.roy54@gmail.com>
2019-11-06 19:18:17 +0530

    final commit

--- src/main/java/com/cg/ems/expenseclaim/web/ExpenseClaimController.java
+++ src/main/java/com/cg/ems/expenseclaim/web/ExpenseClaimController.java
@@ -1,6 +1,10 @@
 package com.cg.ems.expenseclaim.web;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,12 +18,11 @@ import org.springframework.web.client.RestTemplate;
 
 import com.cg.ems.expenseclaim.dto.Client;
 import com.cg.ems.expenseclaim.dto.Employee;
-import com.cg.ems.expenseclaim.dto.Expense;
 import com.cg.ems.expenseclaim.dto.ExpenseClaim;
-import com.cg.ems.expenseclaim.dto.FinanceUser;
-import com.cg.ems.expenseclaim.dto.Project;
+import com.cg.ems.expenseclaim.exception.ExpenseClaimNotFound;
 import com.cg.ems.expenseclaim.service.ExpenseClaimService;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/ems")
 public class ExpenseClaimController {
@@ -35,7 +38,7 @@ public class ExpenseClaimController {
 	}
 	
 	@GetMapping(value="/claim/{claimId}",produces = "application/json")
-	public ExpenseClaim findClaim(@PathVariable int claimId) {
+	public ExpenseClaim findClaim(@PathVariable int claimId) throws ExpenseClaimNotFound{
 		return service.viewClaim(claimId);
 	}
 	
@@ -49,31 +52,31 @@ public class ExpenseClaimController {
 		return service.deleteClaim(claimId);
 		
 	}
-	@GetMapping(value = "/employee",produces = "application/json")
-	public Employee getEmployee(@RequestParam String employeeId) {
-		Employee retrivedEmployee =restTemplate.getForObject("http://product-add-service/product/add/" + employeeId,Employee.class);
+	@GetMapping(value = "/employee/{employeeId}",produces = "application/json")
+	public Employee getEmployee(@PathVariable String employeeId) {
+		Employee retrivedEmployee =restTemplate.getForObject("http://ems-employee-service/employee/id/" + employeeId,Employee.class);
 		return retrivedEmployee;
 	}
 	
 	@GetMapping(value = "/expense",produces = "application/json")
-	public Expense getExpense(@RequestParam int expenseId) {
-		Expense retrivedExpense =restTemplate.getForObject("http://product-add-service/product//" + expenseId,Expense.class);
-		return retrivedExpense;
+	public List<Integer> getExpense() {
+		List<Integer> expenseIds =Arrays.asList(restTemplate.getForObject("http://ems-expense-service/expense/allId",Integer[].class));
+		return expenseIds;
 	}
 	@GetMapping(value = "/client",produces = "application/json")
-	public Client getClient(@RequestParam int clientId) {
-		Client retrivedClient =restTemplate.getForObject("http://product-add-service/product//j" + clientId,Client.class);
-		return retrivedClient;
+	public int getClient() {
+		Client retrivedClient =restTemplate.getForObject("http://product-add-service/product//j",Client.class);
+		return retrivedClient.getClientCode();
 	}
 	@GetMapping(value = "/project",produces = "application/json")
-	public Project getProject(@RequestParam int projectId) {
-		Project retrivedProject =restTemplate.getForObject("http://product-add-service/product//j" + projectId,Project.class);
-		return retrivedProject;
+	public List<Integer> getProject() {
+		List<Integer> projectIds =Arrays.asList(restTemplate.getForObject("http://ems-project-service/project/allId",Integer[].class));
+		return projectIds;
 	}
 	@GetMapping(value = "/finance",produces = "application/json")
-	public FinanceUser getFinance(@RequestParam String financeUserId) {
-		FinanceUser retrivedFinance =restTemplate.getForObject("http://product-add-service/product//j" + financeUserId,FinanceUser.class);
-		return retrivedFinance;
+	public List<String> getFinance() {
+		List<String> financeIds =Arrays.asList(restTemplate.getForObject("http://ems-finance-service/finance/getids",String[].class));
+		return financeIds;
 	}
 	
 }
